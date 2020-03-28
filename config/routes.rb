87fc9d0ae0a50e1582b7361740998919072acc90
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-28 16:09:20 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # 初期のロジックをコメントアウト
+  # デフォルトのルーティングをロジックをコメントアウト
   # get 'sample/index'
   root 'sample#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
